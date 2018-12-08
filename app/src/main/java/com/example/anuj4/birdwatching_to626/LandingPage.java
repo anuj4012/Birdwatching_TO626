64c404e1576ccf752f59f8b347de246c61d14419
anuj4 <anujagr@umich.edu>
2018-12-08 15:21:56 -0500

    added login and bird class

---
+++ app/src/main/java/com/example/anuj4/birdwatching_to626/LandingPage.java
@@ -0,0 +1,83 @@
+package com.example.anuj4.birdwatching_to626;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LandingPage extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonAdd, buttonSearch, buttonImportance;
+    EditText editTextEnterName, editTextEnterDate, editTextEnterZip;
+    EditText editTextEnterImportance, editTextSearchBird;
+
+    TextView textViewLastBird;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonSearch = findViewById(R.id.buttonSearch);
+        buttonImportance = findViewById(R.id.buttonImportance);
+
+        editTextEnterName = findViewById(R.id.editTextEnterName);
+        editTextEnterDate = findViewById(R.id.editTextEnterDate);
+        editTextEnterZip = findViewById(R.id.editTextEnterZip);
+        editTextEnterImportance = findViewById(R.id.editTextEnterImportance);
+        editTextSearchBird = findViewById(R.id.editTextSearchBird);
+
+        textViewLastBird = findViewById(R.id.textViewLastBird);
+
+        buttonAdd.setOnClickListener(this);
+        buttonSearch.setOnClickListener(this);
+        buttonImportance.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        //myRef.setValue("Testing");
+
+        if (v == buttonAdd) {
+
+        String addBird = editTextEnterName.getText().toString();
+
+
+        String addDate = editTextEnterDate.getText().toString();
+        int addDatevalue = Integer.parseInt(addDate);
+
+        String addZip = editTextEnterZip.getText().toString();
+        int addZipvalue = Integer.parseInt(addZip);
+
+        String addImportance = editTextEnterImportance.getText().toString();
+        int addImportancevalue = Integer.parseInt(addImportance);
+
+        Bird newBird = new Bird(addBird,addDatevalue,addZipvalue,addImportancevalue);
+
+        myRef.push().setValue(newBird);
+
+
+        } else if (v == buttonSearch) {
+
+        } else if (v == buttonImportance) {
+
+        }
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/anuj4/birdwatching_to626/MainActivity.java
+++ app/src/main/java/com/example/anuj4/birdwatching_to626/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.anuj4.birdwatching_to626;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends Activity implements View.OnClickListener  {
+
+    Button buttonRegister, buttonLogin;
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
+    Intent intentLandingpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRegister = findViewById(R.id.buttonRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        buttonLogin.setOnClickListener(this);
+        buttonRegister.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == buttonRegister) {
+            mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+        else if(v == buttonLogin) {
+
+        intentLandingpage = new Intent(this, LandingPage.class);
+
+            mAuth.signInWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(MainActivity.this, "logging in: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                                startActivity(intentLandingpage);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(MainActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+        }
+
+
+
     }
 }
