45434a3a29f9ce934334f0a12af5c1ce5bfae89f
anuj4 <anujagr@umich.edu>
2018-12-09 21:07:45 -0500

    final code

--- app/src/main/java/com/example/anuj4/birdwatching_to626/LandingPage.java
+++ app/src/main/java/com/example/anuj4/birdwatching_to626/LandingPage.java
@@ -1,6 +1,8 @@
 package com.example.anuj4.birdwatching_to626;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -16,7 +22,7 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
     Button buttonAdd, buttonSearch, buttonImportance;
     EditText editTextEnterName, editTextEnterDate, editTextEnterZip;
     EditText editTextEnterImportance, editTextSearchBird;
-
+    private FirebaseAuth mAuth;
     TextView textViewLastBird;
 
     @Override
@@ -24,6 +30,7 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
+        mAuth = FirebaseAuth.getInstance();
 
         buttonAdd = findViewById(R.id.buttonAdd);
         buttonSearch = findViewById(R.id.buttonSearch);
@@ -32,7 +39,7 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
         editTextEnterName = findViewById(R.id.editTextEnterName);
         editTextEnterDate = findViewById(R.id.editTextEnterDate);
         editTextEnterZip = findViewById(R.id.editTextEnterZip);
-        editTextEnterImportance = findViewById(R.id.editTextEnterImportance);
+        //editTextEnterImportance = findViewById(R.id.editTextEnterImportance);
         editTextSearchBird = findViewById(R.id.editTextSearchBird);
 
         textViewLastBird = findViewById(R.id.textViewLastBird);
@@ -47,7 +54,9 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
     public void onClick(View v) {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        final DatabaseReference myRef = database.getReference("message");
+
+        String user = mAuth.getCurrentUser().getUid();
 
         //myRef.setValue("Testing");
 
@@ -55,24 +64,90 @@ public class LandingPage extends AppCompatActivity implements View.OnClickListen
 
         String addBird = editTextEnterName.getText().toString();
 
-
         String addDate = editTextEnterDate.getText().toString();
-        int addDatevalue = Integer.parseInt(addDate);
 
         String addZip = editTextEnterZip.getText().toString();
-        int addZipvalue = Integer.parseInt(addZip);
 
-        String addImportance = editTextEnterImportance.getText().toString();
-        int addImportancevalue = Integer.parseInt(addImportance);
 
-        Bird newBird = new Bird(addBird,addDatevalue,addZipvalue,addImportancevalue);
+        Bird newBird = new Bird(addBird,addZip,addDate,user, 0);
 
         myRef.push().setValue(newBird);
 
 
         } else if (v == buttonSearch) {
 
+            String searchBird = editTextSearchBird.getText().toString();
+
+            //zipcode matching
+            myRef.orderByChild("zipcode").equalTo(searchBird).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    String findkey = dataSnapshot.getKey();
+                    Bird findbird = dataSnapshot.getValue(Bird.class);
+
+                    textViewLastBird.setText("The Last Bird sighting is:"  + findbird.birdname + " " + "by user" + " " + findbird.user + " " +  "on" + " " +findbird.date+ " with " + findbird.importance + " importance");
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
         } else if (v == buttonImportance) {
+
+            String findzip = editTextSearchBird.getText().toString();
+
+            myRef.orderByChild("zipcode").equalTo(findzip).limitToLast(1).addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    String findkey = dataSnapshot.getKey();
+                    Bird findbird = dataSnapshot.getValue(Bird.class);
+
+                    myRef.child(findkey).child("importance").setValue(findbird.importance+1);
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
 
 
         }
